f03c2c94b400840db93029e4988b569049250115
equusman <equusman@gmail.com>
2015-05-20 11:10:46 +0700

    Revert "Setting up"
    This reverts commit 1d31244ba7a52e1dc61b68fa7cfda0ac0ebb3f0e.

--- ts/index.php
+++ ts/index.php
@@ -1,8 +1,4 @@
 <?php
-// Khusus deploy ke andramatin
-//	header("Location: http://192.168.0.151/ts/forms/index.php");
-
-// khusus deploy local dan github
-	header("Location: http://localhost:88/Andramatin/ts/forms/index.php");
+	header("Location: http://192.168.0.151/ts/forms/index.php");
 
 ?>
